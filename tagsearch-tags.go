93aa771ddd359f96dd4e479425a341085e73d177
Chris Davison <c.jr.davison@gmail.com>
2025-10-20 16:06:17 +0100

    channels

--- tagsearch-tags.go
+++ tagsearch-tags.go
@@ -4,14 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
+	"sync"
 )
 
 type TagSet map[string]bool
+type TagPair struct {
+	filename string
+	tags     []string
+}
 
 func filesWithSuffix(root, suffix string) []string {
 	var hits []string
@@ -36,7 +42,7 @@ func filesWithSuffix(root, suffix string) []string {
 	return hits
 }
 
-func fileTags(filename string) TagSet {
+func fileTags(filename string, ch chan TagPair) {
 	// Compile the regex: [#@]{1}[a-zA-Z/]
 	// This matches a single # or @ followed by a letter or slash
 	re := regexp.MustCompile(`[#@]{1}[a-zA-Z/]{3,}`)
@@ -58,29 +64,40 @@ func fileTags(filename string) TagSet {
 			file_tags[m] = true
 		}
 	}
-	return file_tags
+	var file_strings []string
+	for tag := range file_tags {
+		file_strings = append(file_strings, tag)
+	}
+	fss := sort.StringSlice(file_strings)
+	fss.Sort()
+
+	ch <- TagPair{filename, fss}
 }
 
 func tagsForFiles(files []string) (map[string][]string, []string) {
-	file_tags := make(map[string]TagSet)
+	file_tags := make(map[string][]string)
 	file_tags_strings := make(map[string][]string)
 
 	all_tag_set := make(map[string]bool)
 
+	channel := make(chan TagPair, len(files))
+	var wg sync.WaitGroup
+
 	for _, filename := range files {
-		ft := fileTags(filename)
-		if len(ft) == 0 {
+		wg.Go(func() {
+			fileTags(filename, channel)
+		})
+	}
+	for i := 0; i < len(files); i++ {
+		ft := <-channel
+		if len(ft.tags) == 0 {
 			continue
 		}
-		file_tags[filename] = ft
-		var ft_strings []string
-		for t := range ft {
-			ft_strings = append(ft_strings, t)
+		log.Println(ft)
+		for _, t := range ft.tags {
 			all_tag_set[t] = true
 		}
-		ft_strings_sorted := sort.StringSlice(ft_strings)
-		ft_strings_sorted.Sort()
-		file_tags_strings[filename] = ft_strings_sorted
+		file_tags[ft.filename] = ft.tags
 	}
 
 	var all_tags []string
