e9a83b8ecffe6f2f7de2aa80a5fde98bc4dd966b
Chris Davison <c.jr.davison@gmail.com>
2025-10-20 09:48:48 +0100

    use hash as tag-starter also
    implement tags and filetags commands

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chrisdavison/tagsearch
+
+go 1.21.0
---
+++ tagsearch-tags.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type TagSet map[string]bool
+
+func filesWithSuffix(root, suffix string) []string {
+	var hits []string
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, e error) error {
+		if e != nil { // permission problems, etc.
+			return nil // skip and continue
+		}
+		if d.IsDir() { // keep recursing
+			return nil
+		}
+		if strings.HasSuffix(path, suffix) {
+			hits = append(hits, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "walk error:", err)
+		os.Exit(1)
+	}
+
+	return hits
+}
+
+func fileTags(filename string) TagSet {
+	// Compile the regex: [#@]{1}[a-zA-Z/]
+	// This matches a single # or @ followed by a letter or slash
+	re := regexp.MustCompile(`[#@]{1}[a-zA-Z/]{3,}`)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	fbuf := bufio.NewScanner(f)
+	file_tags := make(TagSet)
+
+	for fbuf.Scan() {
+		line := fbuf.Text()
+
+		// Find all matches
+		matches := re.FindAllString(line, -1)
+
+		for _, m := range matches {
+			file_tags[m] = true
+		}
+	}
+	return file_tags
+}
+
+func tagsForFiles(files []string) (map[string][]string, []string) {
+	file_tags := make(map[string]TagSet)
+	file_tags_strings := make(map[string][]string)
+
+	all_tag_set := make(map[string]bool)
+
+	for _, filename := range files {
+		ft := fileTags(filename)
+		if len(ft) == 0 {
+			continue
+		}
+		file_tags[filename] = ft
+		var ft_strings []string
+		for t := range ft {
+			ft_strings = append(ft_strings, t)
+			all_tag_set[t] = true
+		}
+		ft_strings_sorted := sort.StringSlice(ft_strings)
+		ft_strings_sorted.Sort()
+		file_tags_strings[filename] = ft_strings_sorted
+	}
+
+	var all_tags []string
+	for tag, _ := range all_tag_set {
+		all_tags = append(all_tags, tag)
+	}
+	sorted := sort.StringSlice(all_tags)
+	sorted.Sort()
+
+	return file_tags_strings, all_tags
+}
+
+func main() {
+	var files []string
+	command := "tags"
+	if len(os.Args) == 1 {
+		files = filesWithSuffix(".", ".md")
+	} else {
+		switch os.Args[1] {
+		case "tags":
+			command = "tags"
+		case "file-tags":
+			command = "filetags"
+		case "filetags":
+			command = "filetags"
+		default:
+			fmt.Fprintln(os.Stderr, "usage: tagsearch [tags|filetags] FILES...")
+			os.Exit(1)
+		}
+		if len(os.Args[2:]) > 0 {
+			for _, arg := range os.Args[2:] {
+				if s, _ := os.Stat(arg); s.IsDir() {
+					files = append(files, filesWithSuffix(arg, ".md")...)
+				} else {
+					files = append(files, arg)
+				}
+			}
+		} else {
+			files = filesWithSuffix(".", ".md")
+		}
+	}
+
+	file_tags, all_tags := tagsForFiles(files)
+
+	switch command {
+	case "tags":
+		fmt.Println(strings.Join(all_tags, "\n"))
+	case "filetags":
+		for ft, tags := range file_tags {
+			fmt.Printf("%s: %s\n", ft, strings.Join(tags, ", "))
+		}
+	}
+}
